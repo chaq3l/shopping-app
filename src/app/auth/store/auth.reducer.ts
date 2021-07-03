9900bd31ff30b9fd3e741c534fd876d294ff6879
chaq3l <karol.stylok@gmail.com>
2021-07-03 17:48:14 +0200

    Store auth actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "../user.model"
+import * as AuthActions from "./auth.actions"
 
 export interface State {
     user?: User
@@ -8,6 +9,27 @@ const initialState : State = {
     user: undefined
 }
 
-export function authReducer(state = initialState, action: any) {
-    return state
+export function authReducer(
+    state = initialState, 
+    action: AuthActions.AuthActions
+    ) {
+        switch(action.type){
+            case AuthActions.LOGIN:
+                const user = new User(
+                    action.payload.email, 
+                    action.payload.userID, 
+                    action.payload.token, 
+                    action.payload.expirationDate)
+                return {
+                    ...state,
+                    user: user
+                }
+            case AuthActions.LOGOUT:
+                return {
+                    ...state,
+                    user: undefined
+                }
+            default:
+                return state
+        }
 }
